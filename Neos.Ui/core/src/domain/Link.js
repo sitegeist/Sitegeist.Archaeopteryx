1be6c2be2824393f4d7b09bdf1a15d815704ac7e
Wilhelm Behncke <behncke@sitegeist.de>
2021-04-27 11:00:23 +0200

    Cleanup build artifacts

--- Neos.Ui/core/src/domain/Link.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-'http://www.example.com';
-'https://www.example.com';
-'node://97c9a6e3-4b50-4559-9f60-b5ad68f25758';
-'asset://97c9a6e3-4b50-4559-9f60-b5ad68f25758';
-'mailto:foo@example.com';
-//# sourceMappingURL=Link.js.map
