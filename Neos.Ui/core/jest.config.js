57f6c0a66db6e6e2d5f17a1899833966d8630851
Christian Pansch <christian.pansch@sitegeist.de>
2025-01-21 14:03:30 +0100

    TASK: Fix jest positional-array-sorter transformation

--- Neos.Ui/core/jest.config.js
+++ Neos.Ui/core/jest.config.js
@@ -3,5 +3,8 @@ module.exports = {
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(t|j)sx?$': 'ts-jest'
-  }
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!(.*@neos-project/positional-array-sorter.*)/)" // Allow Jest to transform this specific module
+  ],
 };
